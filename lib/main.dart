c03ff873d036e52ae77f49df7f1256ecb51ad9dc
SarveshChavan <chavansarvesh64@gmail.com>
2023-07-07 15:06:47 +0530

    Login Page added and some bugs removed

--- lib/main.dart
+++ lib/main.dart
@@ -3,41 +3,79 @@ import 'package:elibrary/providers/homepage_provider.dart';
 import 'package:elibrary/providers/student_provider.dart';
 import 'package:elibrary/providers/book_provider.dart';
 import 'package:elibrary/providers/transaction_provider.dart';
+import 'package:elibrary/screens/login_screen.dart';
+import 'package:elibrary/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (context) => HomePageProvider(index: 0),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => StudentProvider(context: context),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => BookProvider(context: context),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => TransactionProvider(context: context),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => DashboardProvider(context: context),
+      )
+    ],
+    child: const MaterialApp(
+      home: Scaffold(
+        body: MyApp(),
+      ),
+    ),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+
+  bool validToken = false, loading = true;
+
+  Future<void> checkAdmin() async {
+    validToken = await AuthService().checkAdmin(context: context);
+    Future.delayed(
+        Duration(seconds: 5),
+        () => {
+              setState(() {
+                loading = false;
+              })
+            });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkAdmin();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => HomePageProvider(index: 0),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => StudentProvider(context: context),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => BookProvider(context: context),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => TransactionProvider(context: context),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => DashboardProvider(context: context),
-        )
-      ],
-      child: const MaterialApp(
-        home: HomePage(),
-      ),
-    );
+    return loading
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : validToken
+            ? HomePage()
+            : LoginScreen();
   }
 }
